72a5ed6072f130efaaf4aa172879e8900e409ae7
Nikita Y Litvinov <30951223+MENERGO@users.noreply.github.com>
2022-04-15 19:40:49 +0300

    etl_v_2.0

--- etl_events/__main__.py
+++ etl_events/__main__.py
@@ -1,67 +1,18 @@
 import backoff
-import json
 
 from clickhouse_driver import Client
 from kafka import KafkaConsumer
 from kafka.errors import NoBrokersAvailable
-from kafka.structs import OffsetAndMetadata
-from kafka import TopicPartition
 
-from config import settings
-
-
-def create_tables(client: Client) -> None:
-    client.execute("CREATE DATABASE IF NOT EXISTS movies ON CLUSTER company_cluster;")
-    client.execute(
-        """CREATE TABLE IF NOT EXISTS movies.film ON CLUSTER company_cluster(
-            user_uuid String,
-            movie_id String,
-            event Int64,
-            created_at DateTime64
-            ) Engine=MergeTree()
-            ORDER BY created_at;
-     """)
-
-
-@backoff.on_exception(backoff.expo, Exception, max_tries=3)
-def insert_clickhouse(client: Client, data: list) -> None:
-    values: str = str([i for i in data]).lstrip('[').rstrip(']')
-    client.execute(
-        '''
-        INSERT INTO movies.film (
-        user_uuid, movie_id, event, created_at)  VALUES {}
-        '''.format(values)
-    )
-
-
-def etl_process(topic: str, consumer: KafkaConsumer, clickhouse_client: Client) -> None:
-    data: list = []
-    for msg in consumer:
-        data.append(tuple(json.loads(msg.value).values()))
-        if len(data) == 2:
-            insert_clickhouse(clickhouse_client, data)
-            data.clear()
-            tp = TopicPartition(topic, msg.partition)
-            options = {tp: OffsetAndMetadata(msg.offset + 1, None)}
-            consumer.commit(options)
+from etl_events.core.config import settings
+from etl_events.services.services import create_tables, etl_process
+from etl_events.db.clickhouse import clickhouse_client
+from etl_events.db.consumer import consumer
 
 
 @backoff.on_exception(backoff.expo, NoBrokersAvailable)
 def main():
     topic: str = settings.topic
-    consumer = KafkaConsumer(
-        topic,
-        security_protocol="PLAINTEXT",
-        bootstrap_servers=[f"{settings.k_host}:{settings.k_port}"],
-        auto_offset_reset="earliest",
-        enable_auto_commit=True,
-        group_id="$Default",
-        value_deserializer=lambda x: x.decode("utf-8")
-    )
-    clickhouse_client: Client = Client(
-        host=settings.c_host,
-        port=settings.c_port
-    )
     create_tables(clickhouse_client)
     etl_process(topic, consumer, clickhouse_client)
 
---
+++ etl_events/core/config.py
@@ -0,0 +1,33 @@
+__all__ = ['settings']
+
+from pydantic import BaseSettings
+
+
+class KafkaSettings(BaseSettings):
+    """Represents Kafka settings."""
+
+    class Config:
+        env_prefix = 'ETL_'
+    k_host: str = '192.168.5.35'
+    k_port: str = '9092'
+
+
+class ClickHouseSettings(BaseSettings):
+    """Represents ClickHouse settings."""
+
+    class Config:
+        env_prefix = 'ETL_'
+    c_host: str = '192.168.5.35'
+    c_port: str = '9000'
+
+
+class Settings(KafkaSettings, ClickHouseSettings):
+    """Represents ETL settings."""
+
+    class Config:
+        env_prefix = 'ETL_'
+    topic: str = 'film'
+    chunk: int = 2
+
+
+settings = Settings()
