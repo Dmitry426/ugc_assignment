7498d9e81f0e63d03e48dff0b3b3e587de67e836
Nikita Y Litvinov <30951223+MENERGO@users.noreply.github.com>
2022-04-21 12:17:18 +0300

    Fix model and docker-compose

--- ugc_service/models/kafka.py
+++ ugc_service/models/kafka.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+from pydantic import Field
+from typing import Union
+
 from .base import JsonConfig
 
 
 class KafkaEventMovieViewTime(JsonConfig):
     user_uuid: str
     movie_id: str
-    event: str or int
+    event: Union[str, int]
+    created: datetime = Field(default_factory=datetime.utcnow)
