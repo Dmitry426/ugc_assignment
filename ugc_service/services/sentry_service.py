735db984758775be52786869b671a04a7ec1b82f
DmitryK <kachkindmitry@gmail.com>
2022-04-23 22:29:27 +0300

    Sentry init and connection

--- ugc_service/services/sentry_service.py
+++ ugc_service/services/sentry_service.py
@@ -1,8 +1,15 @@
 import sentry_sdk
+from fastapi import FastAPI
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
-from fastapi import FastAPI
+from ugc_service.core.config import CentrySettings
+
+sentry_settings = CentrySettings()
 
 
-def  setry_app(app:FastAPI)->None:
-
+def setry_app(app: FastAPI) -> None:
+    sentry_sdk.init(
+        dsn=sentry_settings.dsn.get_secret_value(),
+        traces_sample_rate=sentry_settings.traces_sample_rate,
+    )
+    SentryAsgiMiddleware(app)
